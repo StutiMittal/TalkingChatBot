be09c4a63eac678a5d2ee3c45d5593cf0bc3e97f
StutiMittal <stutimittal111@gmail.com>
2023-08-25 10:09:38 +0530

    js file change

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector("button");
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.onStart = function () {
+  console.log("Speech Recognition started");
+};
+recognition.onresult = function (event) {
+  console.log(event);
+};
+button.addEventListener("click", () => {});
